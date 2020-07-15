647e021a6924c8a6db84fd104aa523cecaa649a6
Bahman Rashidi <rashidib@vcu.edu>
2020-07-15 16:15:55 -0400

    Update token_login.py

--- token_login.py
+++ token_login.py
@@ -1,7 +1,7 @@
 import requests
 
 username = 'username'
-apitoken = 'sampletoken0123456789'
+apitoken = '!sampletoken0123456789'
 
 #logging in to a server
 login = requests.get('https://sampleapi123.testmysampleapi123.com/', authentication=(username,apitoken))
