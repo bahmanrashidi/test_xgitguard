f5d92a3770e532e8e995cf748f2f714e27805f15
Bahman Rashidi <rashidib@vcu.edu>
2020-07-31 16:25:28 -0400

    Update token_login.py

--- token_login.py
+++ token_login.py
@@ -4,6 +4,7 @@ username = 'username'
 apitoken = '!sampletoken0123456789'
 
 #logging in to a server
-login = requests.get('https://sampleapi123.testmysampleapi123.com/', authentication=(username,apitoken))
+login = requests.get('https://sampleapi123.xgitguardtestmysampleapi123.testdomain/', 
+                     authentication=(username,apitoken))
 
 #end of file
